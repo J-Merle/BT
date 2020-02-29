69dd08510158f73050c34a18946b25ca18dd1e2e
Jeremy Merle <jeremy.merle@protonmail.com>
2020-02-29 11:13:43 +0100

    Create first vulkan instance

--- triangle.cpp
+++ triangle.cpp
@@ -1,5 +1,6 @@
 #include "vulkan/vulkan.h"
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_vulkan.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -20,7 +21,7 @@ class HelloTriangleApplication {
 		SDL_Window* window;
 		SDL_Renderer* renderer;
 		SDL_Event event;
-
+		VkInstance instance;
 
 		void initWindow() {
 			window = SDL_CreateWindow("Triangle", SDL_WINDOWPOS_UNDEFINED,  SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_OPENGL);
@@ -28,7 +29,7 @@ class HelloTriangleApplication {
 		}
 
 		void initVulkan() {
-		
+			createInstance();
 		}
 
 		void mainLoop() {
@@ -48,6 +49,25 @@ class HelloTriangleApplication {
 		void cleanup() {
 			SDL_DestroyRenderer(renderer);
 			SDL_DestroyWindow(window);
+		}
+
+		void createInstance() {
+			VkApplicationInfo appInfo = {};
+			appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+			appInfo.pApplicationName = "Blanco Tower";
+			appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+			appInfo.pEngineName = "No Engine";
+			appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+			appInfo.apiVersion = VK_API_VERSION_1_2;
+
+			VkInstanceCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+			createInfo.pApplicationInfo = &appInfo;
+
+			unsigned int count;
+			if(! SDL_Vulkan_GetInstanceExtensions(window, &count, nullptr))
+				std::cerr << "Could not retrieve Vulkan Instance" << std::endl;
+
 		}
 };
 
